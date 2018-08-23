b1ac37ec50298373479a1d373f7b5e7f870a6dc7
Calcitherin <calcifer.slytherin@icloud.com>
2018-08-23 19:18:34 +0530

    Day 1
    Did Hello World code and Variables use.

---
+++ C++ Basics/HelloWorld.cpp
@@ -0,0 +1,10 @@
+//First program in C++ without IDE
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout<<"Game Over!\n";
+	cout<<"Or is it?\n";
+	return 0;
+}
---
+++ C++ Basics/Variable.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int playerLifes = 3;
+	int playerPotions = 2;
+	int playerBooks = 4;
+
+	int totalItems = playerBooks + playerPotions;
+
+	cout<<totalItems<<endl;
+	cout<<playerLifes<<endl;
+	
+	return 0;
+}
